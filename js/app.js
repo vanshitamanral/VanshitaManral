a261a169f598f266defad7ba99e343f949bda7c2
vanshitamanral <44901776+vanshitamanral@users.noreply.github.com>
2021-08-05 23:46:31 +0530

    commit updates

--- js/app.js
+++ js/app.js
@@ -6,3 +6,8 @@ document.getElementById('btn-right').addEventListener('click',function(){
 document.querySelector('.close').addEventListener('click',function(){
     document.querySelector('.bg-modal').style.display='none';
 });
+
+//downloadresume
+document.getElementById('downloadbutton').addEventListener('click',function(){
+    document.querySelector('.dwd').getAttribute('href');
+});
