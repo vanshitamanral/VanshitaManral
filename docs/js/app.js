d34542b56863f20ad3fb4ca50b4e38bcf3bdf561
vanshitamanral <44901776+vanshitamanral@users.noreply.github.com>
2021-08-05 12:40:08 +0530

    Delete docs directory

--- docs/js/app.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('btn-right').addEventListener('click',function(){
-    document.querySelector('.bg-modal').style.display='flex';
-});
-
-//close
-document.querySelector('.close').addEventListener('click',function(){
-    document.querySelector('.bg-modal').style.display='none';
-});
